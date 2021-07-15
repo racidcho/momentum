44f63fbc17dea3242543b8a49ca244d3a5c65bf5
racidcho <racidcho@gmail.com>
2021-07-15 17:57:53 +0900

    Initial commit

---
+++ js/welcome.js
@@ -0,0 +1,40 @@
+const loginform = document.querySelector("#login-form input");
+const login = document.querySelector("#login-form");
+const name1 = document.querySelector("h1");
+const clearBtn = document.querySelector("#clearBtn");
+
+function onLoginBtnClick(event) {
+    event.preventDefault();
+    const username = loginform.value;
+
+    const HIDDEN_CLASSNAME = "hidden";
+    localStorage.setItem("user", username);
+    login.classList.add(HIDDEN_CLASSNAME);
+    name1.innerText = `${username}님 환영합니다!`;
+    name1.classList.remove(HIDDEN_CLASSNAME);
+    clearBtn.classList.remove("hidden");
+}
+
+const savedUsername = localStorage.getItem("user");
+console.log(savedUsername);
+if (savedUsername === null) {
+    login.classList.remove("hidden");
+    login.addEventListener("submit" , onLoginBtnClick);
+} else {    
+    login.classList.add("hidden");
+    name1.classList.remove("hidden");
+    name1.innerText = `${savedUsername}님 환영합니다!`;
+    clearBtn.classList.remove("hidden");
+}
+
+clearBtn.addEventListener("click" , clearBtnClick);
+
+function clearBtnClick() {
+    event.preventDefault();
+    localStorage.clear();
+    login.classList.remove("hidden");
+    name1.classList.add("hidden");
+    clearBtn.classList.add("hidden");
+    login.addEventListener("submit" , onLoginBtnClick);
+    loginform.innerText = "";
+}
