39e63f51a58b770492be35a8138af108aa9f8aa7
racidcho <racidcho@gmail.com>
2021-07-15 18:00:02 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,8 @@ const clock = document.querySelector("h2#clock");
 
 function clocks() {
     const day = new Date; 
-    
-    if (day.getSeconds() === 0) {
-        const zero = day.getSeconds() +0 ;    
-    }
-    clock.innerText = `${day.getHours()}:${day.getMinutes()}:${zero}`;
+   
+    clock.innerText = `${day.getHours()}:${day.getMinutes()}:${day.getSeconds()}`;
 }
 clocks();
 setInterval(clocks, 1000);
