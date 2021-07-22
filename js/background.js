e2d605d6f085220a0baed1c8e05e8243620ee08c
racidcho <racidcho@gmail.com>
2021-07-22 15:30:13 +0900

    mobile

--- js/background.js
+++ js/background.js
@@ -1,10 +1,17 @@
-const images = ["0.jpg","1.jpg","2.jpg","3.jpg"];
-
-const todayImg = images[Math.floor(Math.random()*images.length)];
+const images = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg"];
 
 const bgImg = document.createElement("img");
 
-bgImg.src = `img/${todayImg}`;
+function randomImg() {
+  const div = document.createElement("div");
+  const todayImg = images[Math.floor(Math.random()*images.length)];
+  bgImg.src = `img/${todayImg}`;
+  document.body.appendChild(bgImg);
+  document.body.appendChild(div);
+  
+  bgImg.classList.add("bgImg");
+  div.classList.add("mobileBg");
 
-document.body.appendChild(bgImg);
-bgImg.classList.add("bgImg");
+}
+randomImg();
+setInterval(randomImg, 60000);
