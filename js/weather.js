14c9fa37af495a71005420491bdf905d42ea0e95
racidcho <65842648+racidcho@users.noreply.github.com>
2021-07-26 20:20:45 +0900

    weather

--- js/weather.js
+++ js/weather.js
@@ -14,8 +14,10 @@ function onGeoOk(position) {
    
       weatherDiv.innerText = "현재 날씨는 흐립니다.";
       weatherDiv2.innerText = `현재온도는 ${tempFinal} 입니다.`;
-     
-    
+    }
+    else {
+      weatherDiv.innerText = data.weather[0].main;
+      weatherDiv2.innerText = `현재온도는 ${tempFinal} 입니다.`;
     }
     
   });
